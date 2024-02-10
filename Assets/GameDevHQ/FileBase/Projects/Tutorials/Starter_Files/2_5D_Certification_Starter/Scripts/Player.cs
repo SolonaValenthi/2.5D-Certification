c7f6c5d0afa9a3862fd6cd9dc6d357527823fe58
Nikolai Nelson <nikolainelson@comcast.net>
2024-02-10 04:26:08 -0800

    player model and animations implemented.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -9,8 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _gravityForce;
-
+    [SerializeField] private Animator _anim;
+    [SerializeField] private GameObject _playerModel;
+ 
     private float _yVelocity;
+    private float _rotationSpeed;
     private PlayerInputActions _input;
     
     Vector3 _velocity;
@@ -35,6 +38,7 @@ public class Player : MonoBehaviour
     {
         float move = _input.Player.Movement.ReadValue<float>();
         _velocity = new Vector3(0, 0, move) * _moveSpeed;
+        _anim.SetFloat("Speed", Mathf.Abs(move));
 
         if (!_controller.isGrounded)
         {
@@ -42,12 +46,35 @@ public class Player : MonoBehaviour
         }
 
         _velocity.y = _yVelocity;
+        RotateModel(move);
         _controller.Move(_velocity * Time.deltaTime);
+
+        if (_controller.isGrounded)
+            _anim.SetBool("Jump", false);
+    }
+
+    private void RotateModel(float movement)
+    {
+        float rotation;
+
+        if (movement < 0)
+        {
+            rotation = Mathf.SmoothDampAngle(_playerModel.transform.eulerAngles.y, 180, ref _rotationSpeed, 0.1f);
+            _playerModel.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+        else if (movement > 0)
+        {
+            rotation = Mathf.SmoothDampAngle(_playerModel.transform.eulerAngles.y, 0, ref _rotationSpeed, 0.1f);
+            _playerModel.transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }       
     }
 
     private void Jump(InputAction.CallbackContext context)
     {
         if (_controller.isGrounded)
+        {
             _yVelocity = _jumpForce;
+            _anim.SetBool("Jump", true);
+        }  
     }
 }
